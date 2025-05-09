9abe5b64dc3564fb383a0ff22c5264c333fee10f
CodeurDHonneur <kkodjoaugustin@gmail.com>
2025-05-09 20:36:27 +0100

    amélioration css range

--- src/components/RangeComponent.tsx
+++ src/components/RangeComponent.tsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React from 'react';
+import styles from "../styles/RangeComponent.module.css";
+
 
 export default function RangeComponent() {
     const [valueInput, setValueInput] = React.useState(0)
@@ -6,31 +8,40 @@ export default function RangeComponent() {
     return (
         <>
             <div>
-                <h3>title</h3>
-                <div>
-                    <span style={{
-                        padding: "5px",
-                        border: "3px solid #f2f4f5",
-                        backgroundColor: "white",
-                        fontSize: "20px",
-                    }}>{valueInput}</span>
-                    <span>unité</span>
+                <div className={styles.description}>
+                    <h3>title</h3>
+                    <div>
+                        <p style={{
+                            padding: "5px",
+                            border: "3px solid #f2f4f5",
+                            backgroundColor: "white",
+                            fontSize: "20px",
+                            width: "30px"
+                        }}>{valueInput}</p>
+                        <p>unité</p>
+                    </div>
                 </div>
             </div>
-            <input
-                list="tickmarks"
-                value={valueInput}
-                onChange={(e) => setValueInput(Number(e.target.value))}
-                type="range"
-                max="200"
-                min="-200"
-                style={{
-                    width: "450px",
-                    appearance: "none",
-                    height: "5px",
-                    background: "#f2f4f5"
-                }}
-            />
+            <div className={styles.containerInputRange}>
+                <input
+                    list="tickmarks"
+                    value={valueInput}
+                    onChange={(e) => setValueInput(Number(e.target.value))}
+                    type="range"
+                    max="200"
+                    min="-200"
+                    style={{
+                        width: "450px",
+                        appearance: "none",
+                        height: "5px",
+                        background: "#f2f4f5",
+                        zIndex: 1,
+                        position: "relative",
+                    }}
+                />
+                <div className={styles.rangeCenterMarker}></div>
+            </div>
+
 
         </>
     )
--- src/features/ShadowConfig/features/ShadowContent.tsx
+++ src/features/ShadowConfig/features/ShadowContent.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ShadowList from './ShadowList'
-
+import styles from "../styles/ShadowContent.module.css"
 export default function ShadowContent() {
   return (
-    <div>
+    <div className={styles.container}>
       <ShadowList />
     </div>
   )
